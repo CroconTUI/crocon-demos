5dbebffdee213f66bb170f5f48f4228a24f13769
Dmitry Tretyakov <tinelix@mail.ru>
2026-04-22 17:32:00 +0700

    Fix crash

--- barebns1/src/main.c
+++ barebns1/src/main.c
@@ -3,14 +3,15 @@
 
 int main() {
 	
-	CROCVERSION* crocon_ver;
-
-	crocon_getver(crocon_ver);
+	CROCVERSION crocon_ver;
+	int crocon_result;
 	
-	printf("Crocon Test Application\r\n\r\n");
+	crocon_result = crocon_getver(&crocon_ver);
+	
+	printf("\r\nCrocon Test Application\r\n");
 	printf(
 		"Version: %d.%d.%d\r\n\r\n", 
-		crocon_ver->major, crocon_ver->minor, crocon_ver->patch
+		crocon_ver.major, crocon_ver.minor, crocon_ver.patch
 	);
 	
 	crocon_initscr();
